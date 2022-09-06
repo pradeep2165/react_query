481177067d120b512a60513b5f6580ef38096144
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-06 17:05:58 +0530

    router implemented

---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const RQSuperHeroesPage = () => {
+  return (
+    <div>
+      <h1>RQ SuperHeroes Page</h1>
+    </div>
+  );
+};
---
+++ src/components/SuperHeroes.page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SuperHeroesPage = () => {
+  return (
+    <div>
+      <h1>SuperHeroes Page</h1>
+    </div>
+  );
+};
