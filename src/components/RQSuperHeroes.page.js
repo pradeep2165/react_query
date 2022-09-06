12b7982fbdfdc4e9dc0e92c7286046de4a16b7b4
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-06 17:51:50 +0530

    data fetched using react-query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import { useQuery } from "react-query";
+import axios from "axios";
+
 export const RQSuperHeroesPage = () => {
+  const { isLoading, data } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
   return (
     <div>
       <h1>RQ SuperHeroes Page</h1>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
     </div>
   );
 };
