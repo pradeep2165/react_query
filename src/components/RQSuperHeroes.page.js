858db9f32c9f844a77a29fbd5bb6050768b21f6c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-06 18:07:34 +0530

    error handled in new ways

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -5,10 +5,17 @@ import axios from "axios";
 
 export const RQSuperHeroesPage = () => {
   const fetchSuperHeroes = () => {
-    return axios.get("http://localhost:4000/superheroes");
+    return axios.get("http://localhost:4000/superheroes1");
   };
 
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading....</h2>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
   return (
     <div>
       <h1>RQ SuperHeroes Page</h1>
