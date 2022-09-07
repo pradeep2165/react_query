ebb705d5d6197748b9c97c0ddf4802f3b179aed5
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-07 18:33:24 +0530

    used useMutation for post query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 // import axios from "axios";
-import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+import { useAddSuperHeroData, useSuperHeroesData } from "../hooks/useSuperHeroesData";
 import { Link } from "react-router-dom";
 
 export const RQSuperHeroesPage = () => {
@@ -8,6 +8,8 @@ export const RQSuperHeroesPage = () => {
   //   return axios.get("http://localhost:4000/superheroes");
   // };
   //callback activity
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
   const onSuccess = (data) => {
     console.log("perform side effect after data fetching", data);
   };
@@ -18,6 +20,13 @@ export const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError);
 
   // console.log(isLoading, isFetching);
+  const { mutate: addHero } = useAddSuperHeroData();
+
+  const handleAddHeroClick = () => {
+    console.log({ name, alterEgo });
+    const hero = { name, alterEgo };
+    addHero(hero);
+  };
 
   if (isLoading || isFetching) {
     return <h2>Loading....</h2>;
@@ -28,6 +37,11 @@ export const RQSuperHeroesPage = () => {
   return (
     <div>
       <h1>RQ SuperHeroes Page</h1>
+      <div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={alterEgo} onChange={(e) => setAlterEgo(e.target.value)} />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
       <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => {
         return (
