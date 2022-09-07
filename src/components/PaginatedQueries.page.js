092f8fdb8b2af8970cceabddc20469d10230ae7f
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-07 16:32:05 +0530

    pagination working

--- src/components/PaginatedQueries.page.js
+++ src/components/PaginatedQueries.page.js
@@ -1,12 +1,14 @@
 import { useQuery } from "react-query";
 import axios from "axios";
+import { useState } from "react";
 
-const fetchColor = () => {
-  return axios.get(`http://localhost:4000/colors`);
+const fetchColor = (pageNumber) => {
+  return axios.get(`http://localhost:4000/colors?_limit=2&_page=${pageNumber}`);
 };
 
 export const PaginatedQuriesPage = () => {
-  const { isLoading, isError, error, data } = useQuery("colors", fetchColor);
+  const [pageNumber, setPageNumber] = useState(1);
+  const { isLoading, isError, error, data } = useQuery(["colors", pageNumber], () => fetchColor(pageNumber));
 
   if (isLoading) {
     return <h2>Loading...</h2>;
@@ -28,6 +30,14 @@ export const PaginatedQuriesPage = () => {
             </div>
           );
         })}
+      </div>
+      <div>
+        <button onClick={() => setPageNumber((page) => page - 1)} disabled={pageNumber === 1}>
+          Prev Page
+        </button>
+        <button onClick={() => setPageNumber((page) => page + 1)} disabled={pageNumber === 4}>
+          Next Page
+        </button>
       </div>
     </>
   );
